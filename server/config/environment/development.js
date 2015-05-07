f39931524845aa0c5a5de411792297e5850e914e
Paul Garcia <pbrain19@gmail.com>
2015-05-07 12:48:34 -0400

    started the project

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://pbrain19:321654987p@ds037508.mongolab.com:37508/gymbud'
+  },
+
+  seedDB: true
+};
---
+++ server/config/environment/production.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Production specific configuration
+// =================================
+module.exports = {
+  // Server IP
+  ip:       process.env.OPENSHIFT_NODEJS_IP ||
+            process.env.IP ||
+            undefined,
+
+  // Server port
+  port:     process.env.OPENSHIFT_NODEJS_PORT ||
+            process.env.PORT ||
+            8080,
+
+  // MongoDB connection options
+  mongo: {
+    uri:    process.env.MONGOLAB_URI ||
+            process.env.MONGOHQ_URL ||
+            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+            'mongodb://pbrain19:321654987p@ds037508.mongolab.com:37508/gymbud'
+  }
+};
