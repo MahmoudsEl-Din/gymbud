b073abfe0e3a0a9c609cc6573c3e544c62a4aa81
Paul Garcia <pbrain19@gmail.com>
2015-05-08 15:23:49 -0400

    started model validations and schemas. Mainly activity is done. Moving to work on articles.

--- server/api/activity/activity.model.js
+++ server/api/activity/activity.model.js
@@ -4,9 +4,24 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var ActivitySchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  title: {
+    type: String,
+    required:true
+  },
+  story: {
+    type: String,
+    required:true
+  },
+  active: Boolean,
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  },
+  dateModified: Date,
+  _author: {
+    type:Schema.Types.ObjectId,
+    ref:'user'
+  }
 });
 
 module.exports = mongoose.model('Activity', ActivitySchema);
--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -13,7 +13,11 @@ var UserSchema = new Schema({
   },
   hashedPassword: String,
   provider: String,
-  salt: String
+  salt: String,
+  active: {
+    type: Boolean,
+    default: true
+  }
 });
 
 /**
